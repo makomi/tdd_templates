04d0bdd7a011f94ed2fa2a641a2af0b9cca7fef5
Matthias Kolja Miehl <code@makomi.net>
2017-08-10 21:35:03 +0200

    C++_CppUTest: Create as a copy of 'C_CppUTest'

---
+++ C++_CppUTest/test/UnitTests.cc
@@ -0,0 +1,49 @@
+#include <CppUTest/TestHarness.h>
+#include <CppUTest/CommandLineTestRunner.h>
+#include <CppUTestExt/MockSupport.h>
+
+extern "C" {
+#include "../src/module.h"
+}
+
+//------------------------------------------------------------------------------
+// this is where the functions are tested
+
+TEST_GROUP(FirstTestGroup)
+{
+	void setup()
+	{
+	}
+
+	void teardown()
+	{
+	}
+};
+
+TEST(FirstTestGroup, FirstTest)
+{
+	// given
+
+	// when
+
+	// then
+	FAIL("Fail me!");
+}
+
+TEST(FirstTestGroup, SecondTest)
+{
+	// given
+
+	// when
+
+	// then
+	STRCMP_EQUAL("hello", "world");
+}
+
+//------------------------------------------------------------------------------
+
+int main(int ac, char** av)
+{
+	return CommandLineTestRunner::RunAllTests(ac, av);
+}
+
